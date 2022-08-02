fbc2a5d4741a3a4606c31192b6b4584a29706ebe
amigin <amigin@gmail.com>
2022-08-02 16:59:22 +0300

    Fixing some problems

--- src/auto_login_token.rs
+++ src/auto_login_token.rs
@@ -13,7 +13,7 @@ impl AutoLoginToken {
         Self { client_id, expires }
     }
 
-    pub fn to_string(&self, key: AesKey) -> String {
+    pub fn to_string(&self, key: &AesKey) -> String {
         let mut result = Vec::new();
 
         prost::Message::encode(self, &mut result).unwrap();
@@ -21,7 +21,7 @@ impl AutoLoginToken {
         hex::encode(result)
     }
 
-    pub fn parse(token: &str, key: AesKey) -> Self {
+    pub fn parse(token: &str, key: &AesKey) -> Self {
         let encrypted = hex::decode(token).unwrap();
         let bytes = key.encrypt(encrypted.as_slice());
         prost::Message::decode(bytes.as_slice()).unwrap()
