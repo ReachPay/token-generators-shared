901df64b93dc9a77d33c9f0229ff04e6b60a9490
amigin <amigin@gmail.com>
2022-08-03 09:23:41 +0300

    Added some features to Token

--- src/auto_login_token.rs
+++ src/auto_login_token.rs
@@ -1,4 +1,5 @@
 use encryption::aes::AesKey;
+use rust_extensions::date_time::DateTimeAsMicroseconds;
 
 #[derive(::prost::Message)]
 pub struct AutoLoginToken {
@@ -21,9 +22,25 @@ impl AutoLoginToken {
         hex::encode(result)
     }
 
-    pub fn parse(token: &str, key: &AesKey) -> Self {
-        let encrypted = hex::decode(token).unwrap();
-        let bytes = key.encrypt(encrypted.as_slice());
-        prost::Message::decode(bytes.as_slice()).unwrap()
+    pub fn parse(token: &str, key: &AesKey) -> Option<Self> {
+        let encrypted = hex::decode(token);
+        if encrypted.is_err() {
+            return None;
+        }
+
+        let bytes = key.encrypt(encrypted.unwrap().as_slice());
+
+        let result: Result<Self, _> = prost::Message::decode(bytes.as_slice());
+
+        if result.is_err() {
+            return None;
+        }
+
+        Some(result.unwrap())
+    }
+
+    pub fn is_expired(&self, now: DateTimeAsMicroseconds) -> bool {
+        let expires = DateTimeAsMicroseconds::new(self.expires);
+        expires.unix_microseconds < now.unix_microseconds
     }
 }
